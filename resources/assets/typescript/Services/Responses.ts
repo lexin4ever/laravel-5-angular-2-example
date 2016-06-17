1aa59be6e1ef6b2f8d576d19f2c6bf0c5240b499
Alexey Kuznetsov <lexin4ever@gmail.com>
2016-06-17 11:22:05 +0300

    move keyframes to separate file, type on responses

--- resources/assets/typescript/Services/Responses.ts
+++ resources/assets/typescript/Services/Responses.ts
@@ -4,9 +4,9 @@ export interface APIResponse {
 }
 
 export class OkResponse implements APIResponse{
-    constructor(public status: String, public message?: String = '') {}
+    constructor(public status: String, public message: String = '') {}
 }
 
 export class FailResponse implements APIResponse {
-    constructor(public status: String, public error: String, public message?: String = '') { }
+    constructor(public status: String, public error: String, public message: String = '') { }
 }
